d2748f67a48e34f83f6161240de5042044e6e8db
sriramkv1409 <sriramkv1409@gmail.com>
2025-07-15 23:32:50 +0530

    Junit testing

--- src/test/java/com/example/springbbootfirst/Controllers/EmployeeControllerTest.java
+++ src/test/java/com/example/springbbootfirst/Controllers/EmployeeControllerTest.java
@@ -1,6 +1,7 @@
 package com.example.springbbootfirst.Controllers;
 
 import com.example.springbbootfirst.Models.RegisterDetails;
+import com.example.springbbootfirst.Models.Roles;
 import com.example.springbbootfirst.Services.EmployeeService;
 import org.aspectj.lang.annotation.Before;
 import org.junit.jupiter.api.BeforeEach;
@@ -8,9 +9,11 @@ import org.junit.jupiter.api.Test;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.mockito.MockitoAnnotations;
+import org.springframework.data.relational.core.sql.When;
 
 import java.util.Arrays;
 import java.util.List;
+import java.util.Set;
 
 import static org.junit.jupiter.api.Assertions.*;
 import static org.mockito.Mockito.when;
@@ -41,4 +44,22 @@ class EmployeeControllerTest {
         List<RegisterDetails> result = employeeService.getAllEmployees();
         assertEquals(2,result.size());
     }
+
+    @Test
+    void testGetEmployeeById(){
+        int empId = 1;
+        RegisterDetails mockEmployee = new RegisterDetails();
+        mockEmployee.setEmpId(empId);
+        mockEmployee.setUserName("sanz");
+
+        when(employeeService.getEmployeeById(empId)).thenReturn(mockEmployee);
+        RegisterDetails result = employeeController.getEmployeeById(empId);
+
+        assertNotNull(result);
+        assertEquals(mockEmployee.getEmpId(),result.getEmpId());
+        assertEquals(mockEmployee.getUserName(),result.getUserName());
+
+    }
+
+    
 }
